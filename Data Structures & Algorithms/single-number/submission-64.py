67666b84a52f76ec4fe74ccfa10eff25c7166a86
Trey DeWald <36147174+treydewald@users.noreply.github.com>
2026-05-23 14:40:08 -0400

    Add: single-number - submission-64

---
+++ Data Structures & Algorithms/single-number/submission-64.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                i += 2
+            else:
+                return nums[i]
+        return nums[i]
